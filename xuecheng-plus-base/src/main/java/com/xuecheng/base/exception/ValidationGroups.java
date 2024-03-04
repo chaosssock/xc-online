1817bb225702ea4f9e5dcf0900b83e46470d07a6
chaosssock <huangchuyao@foxmail.com>
2024-03-04 20:56:08 +0800

    JSR303校验

---
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/ValidationGroups.java
@@ -0,0 +1,13 @@
+package com.xuecheng.base.exception;
+
+/**
+ * @Auther: chaosssock
+ * @Date: 2024/3/4 20:49
+ * @Description: 用于分组校验，定义一些常用的组
+ */
+public class ValidationGroups {
+    public interface Inster{};
+    public interface Update{};
+    public interface Delete{};
+
+}
