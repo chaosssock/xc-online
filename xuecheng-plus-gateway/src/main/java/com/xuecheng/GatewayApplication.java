ba40cb70e4d217717fb3442f823bc04e1445e74d
chaosssock <huangchuyao@foxmail.com>
2024-03-19 22:18:49 +0800

    网关gateway

---
+++ xuecheng-plus-gateway/src/main/java/com/xuecheng/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.xuecheng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: chaosssock
+ * @Date: 2024/3/18 22:57
+ * @Description: 请输入描述
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
